bc4762f734c74479fa8f99c327f22061a3f8a436
n1b11 <nburd@clarku.edu>
2026-04-04 15:26:56 -0400

    minor tweaks

--- frontend/components/LandingPage.tsx
+++ frontend/components/LandingPage.tsx
@@ -1,9 +1,10 @@
 "use client";
 import getQuest from "@/lib/api";
+import getDisplayTime from "@/lib/utils";
 import { useState } from "react";
 
 export default function LandingPage() {
-  const [time, setTime] = useState<number | null>(null);
+  const [time, setTime] = useState<number>(0);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -12,18 +13,18 @@ export default function LandingPage() {
         <input
           className="border-2 border-gray-300 rounded-md p-1 w-16 step={15}:"
           type="number"
-          value={time ?? ""}
+          value={time}
           step={15}
-          
+          min={0}
           onChange={(e) =>
-            setTime(e.target.value === "" ? null : Number(e.target.value))
+            setTime(e.target.value === "" ? 0: Number(e.target.value))
           }
         />
         <h1>minutes</h1>
       </div>
 
       <button 
-        onClick={() => getQuest(time ?? 0)}
+        onClick={() => getQuest(time)}
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 cursor-pointer click:bg-blue-700">
         Find Me Something To Do
       </button>
