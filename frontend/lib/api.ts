c3c292bb2113ca1dacc327596ba5ccea70fdda5b
n1b11 <nburd@clarku.edu>
2026-04-04 16:06:04 -0400

    update base url

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
-
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 const getQuest = async (time: number, location: [number, number]) => {
+    console.log(BASE_URL);
     try{
         const body = {
             "time": time, 
             "location": location
         }
-        const response =  await axios.post(`${process.env.BACKEND_API_URL}/quests`, body);
+        const response =  await axios.post(`${BASE_URL}/quest`, body);
         const data = response.data;
         return data;
     } catch (error) {
