1d4268f5f625ccfa47099f28b8189a52701f3407
Christopher Siems <christophersiems@protonmail.com>
2026-04-04 18:32:25 -0400

    db integration

--- backend/server/routes/quest.py
+++ backend/server/routes/quest.py
@@ -1,18 +1,22 @@
-from json import load
+from ast import literal_eval
 from random import choices
 
 from fastapi import APIRouter
 
 from backend.server.models.quest import Quest
 from backend.server.models.quest_request import QuestRequest
+from database import get_available_quests
 
 router = APIRouter()
 
 
 @router.post("/quest", response_model=Quest)
 async def quest(quest_request: QuestRequest):
-  quest = None
-  with open("backend/server/dummy_quests.json") as f:
-    quest = choices([Quest(**quest) for quest in load(f)])[0]
-
-  return quest
+  quest = choices(get_available_quests(quest_request.minutes))[0]
+  return Quest(
+    title=quest["title"],
+    link=quest["link"],
+    start_time=quest["start_time"],
+    end_time=quest["end_time"],
+    location=literal_eval(quest["location"]),
+  )
