18ac13a894aca275424f50ad1808b046b100a838
Christopher Siems <christophersiems@protonmail.com>
2026-04-04 21:47:46 -0400

    working

--- backend/server/routes/quest.py
+++ backend/server/routes/quest.py
@@ -11,13 +11,20 @@ router = APIRouter()
 
 @router.post("/quest", response_model=Quest)
 async def quest(quest_request: QuestRequest):
+  print("location", str(quest_request))
+  quest = choices(
+    get_available_quests(
+      quest_request.minutes,
+      quest_request.location[0],
+      quest_request.location[1],
+      db_path="backend/data/quests.db",
+    )
+  )[0]
+  print(quest)
   return Quest(
-    **choices(
-      get_available_quests(
-        quest_request.minutes,
-        quest_request.location[0],
-        quest_request.location[1],
-        db_path="backend/data/quests.db",
-      )
-    )[0]
+    start_time=quest["start_time"],
+    end_time=quest["end_time"],
+    location=quest["location"],
+    title=quest["title"],
+    link=quest["link"],
   )
