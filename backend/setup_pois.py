a16e219ce037cf931842c05667b4f64072addb9d
Christopher Siems <christophersiems@protonmail.com>
2026-04-05 04:36:31 -0400

    embeddings for POIs

--- backend/setup_pois.py
+++ backend/setup_pois.py
@@ -1,9 +1,14 @@
 from json import load
 
 from database import create_POI
+from scraper.embed import attach_embeddings_to_quests
 
+quests = []
 with open("POIs.json") as f:
-  for quest in load(f):
-    create_POI(
-      **{k: v for k, v in quest.items() if k != "id"}, db_path="backend/data/quests.db"
-    )
+  quests = load(f)
+
+quests = attach_embeddings_to_quests(quests)
+for quest in quests:
+  create_POI(
+    **{k: v for k, v in quest.items() if k != "id"}, db_path="backend/data/quests.db"
+  )
