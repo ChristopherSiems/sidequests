88bcb03966f49788ae6927d1cf09cacf91732413
Christopher Siems <christophersiems@protonmail.com>
2026-04-05 06:39:01 -0400

    updated origins

--- backend/server/main.py
+++ backend/server/main.py
@@ -25,7 +25,8 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:3000", "https://sidequests-ten.vercel.app/"],
+  # allow_origins=["http://localhost:3000", "https://sidequests-ten.vercel.app/"],
+  allow_origins=["*"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
