1019c32d72ff550bad98f1e51ae210eb6ebf4dac
elliotgedwards <elliotgedwards@gmail.com>
2026-04-05 07:04:56 -0400

    Fix pydantic error

--- backend/server/models/quest.py
+++ backend/server/models/quest.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 class Quest(BaseModel):
   location: str
   title: str
-  start_time: int | None = None
-  end_time: int | None = None
+  start_time: int | str | None = None
+  end_time: int | str | None = None
   link: str | None = None
   embedding: list[float] = Field(default_factory=list)
